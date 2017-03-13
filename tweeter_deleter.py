62bd562bba2f09d91c4b3b62200c46cee57812f2
Trevor Davies <trevordavies095@gmail.com>
2017-03-12 22:46:30 -0400

    first commit

---
+++ tweeter_deleter.py
@@ -0,0 +1,71 @@
+"""
+Name       : Loren Davies
+Program    : Tweeter Deleter
+Description: Deletes tweets based on specified keywords.
+Date       : March 12th, 2017
+"""
+
+from twython import Twython
+from twython import TwythonError
+import argparse
+import time
+
+def term_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("twitter_handle", type=str, help="twitter handle")
+    parser.add_argument("keyword_file", type=str, help="text file containing keywords")
+    return parser.parse_args()
+
+
+def keyword_remover(twitter, username, file):
+	f = open(file, "r")
+	words = []
+	ids = []
+    count = 0
+    deleted = 0
+
+    for line in f:
+    	line = line.strip()
+    	words.append(line)
+    f.close()
+
+    print("Deleting tweets...")
+
+    for i in range(0, 16):
+    	user_timeline = twitter.get_user_timeline(screen_name=username, count=200, include_retweets=False,
+                                                  max_id=ids[-1])
+
+    	for tweet in user_timeline:
+    		if count == 0:
+    			ids.append(tweet['id'])
+    		for i in range(0, len(words)):
+    			if words[i] in tweet['text'].lower():
+    				try:
+    					twitter.destroy_status(id=tweet['id'])
+                        deleted += 1
+                        continue
+                    except TwythonError:
+                    	print("Tweet already deleted...")
+                else:
+                	ids.append(tweet['id'])
+
+            count += 1
+
+        print("Tweets deleted: " + str(deleted))
+        print("Tweets checked: " + str(count))
+        time.sleep(300)
+
+    print("Tweets deleted!")
+
+
+def main():
+	consumer_key        = ""	# Twitter app credentials
+	consumer_key_secret = ""
+	access_token        = ""
+	access_token_secret = ""
+	args = term_args()
+
+	twitter = Twython(consumer_key, consumer_key_secret, access_token, access_token_secret)
+
+	keyword_remover(twitter, args.twitter_handle, args.keyword_file)
